2858038423d4db4ff7d5d0044d7486cefe49ef09
hkr <hkrhasan8010@gmail.com>
2020-12-16 22:02:50 +0530

    just Starting

---
+++ src/utilities.js
@@ -0,0 +1,19 @@
+export const drawRect = (detections, ctx) => {
+    detections.forEach(prediction => {
+        // Get Prediction result 
+        const [x,y,width,height] = prediction['bbox']
+        const text = prediction['class']
+
+        // set styling
+        const color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        ctx.strokeSylt = color
+        ctx.font = '18px Arial'
+        ctx.fillStyle = color
+
+        // Draw rectangles and text
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+    })
+}
